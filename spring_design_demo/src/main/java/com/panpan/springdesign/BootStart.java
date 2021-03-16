dd956f1acdfd947c38a795bfa031ed9e8ccbb8ca
hoganne <1558420533@qq.com>
2021-03-16 21:46:15 +0800

    know

--- spring_design_demo/src/main/java/com/panpan/springdesign/BootStart.java
+++ spring_design_demo/src/main/java/com/panpan/springdesign/BootStart.java
@@ -1,11 +1,19 @@
 package com.panpan.springdesign;
 import com.panpan.springdesign.entiy.Blog;
 import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.mapping.Environment;
+import org.apache.ibatis.session.Configuration;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.apache.ibatis.transaction.TransactionFactory;
+import org.apache.ibatis.transaction.jdbc.JdbcTransactionFactory;
+
+import javax.sql.DataSource;
+import javax.xml.crypto.Data;
 import java.io.IOException;
 import java.io.InputStream;
+import java.sql.DriverManager;
 
 /**
  * @Description TODO
@@ -28,4 +36,19 @@ public class BootStart {
         }
         System.out.println(blog);
     }
+
+//    public static void main(String[] args) {
+//        DataSource dataSource = BlogDataSourceFactory.getBlogDataSource();
+//        TransactionFactory transactionFactory = new JdbcTransactionFactory();
+//        Environment environment = new Environment("development", transactionFactory, dataSource);
+//        Configuration configuration = new Configuration(environment);
+//        configuration.addMapper(BlogMapper.class);
+//        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(configuration);
+//    }
+//
+//    private static class BlogDataSourceFactory {
+//        public static DataSource getBlogDataSource() {
+//            return null;
+//        }
+//    }
 }
