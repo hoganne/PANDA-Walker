49155a57748870dc2bcd42ae1c118b208afa2c8a
hoganne <48193339+hoganne@users.noreply.github.com>
2021-03-25 10:57:02 +0800

    know

---
+++ springCloud/springCloudpayment/src/main/java/com/example/payment/Application.java
@@ -0,0 +1,13 @@
+package com.example.payment;
+
+/**
+ * @Description
+ * @Author xupan
+ * @Date2021/3/24 17:15
+ * @Version V1.0
+ **/
+public class Application {
+    public static void main(String[] args) {
+        System.out.println("haha");
+    }
+}
